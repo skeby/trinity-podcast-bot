244700871cb69095c9990dee5f73a0ee022de237
Skeby <103804946+Skeby@users.noreply.github.com>
2024-01-25 21:24:16 +0100

    fix: change export

--- api/commands/cmd_start.js
+++ api/commands/cmd_start.js
@@ -1,5 +1,7 @@
-export default (ctx) => {
+const cmd_start = (ctx) => {
   ctx.reply(
     "Welcome to Trinity Podcast Bot/nSend /soldier-details to view your details"
   );
 };
+
+export { cmd_start };
