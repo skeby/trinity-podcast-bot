2b1e91f4e15112f3f0195429bba23a00392897ac
Skeby <103804946+Skeby@users.noreply.github.com>
2024-01-25 21:34:41 +0100

    Revert "fix: export webhook"
    This reverts commit 85bbf6c3f8dfeb3b3ce5719aacf8a81a9c0ee72d.

--- api/webhook.js
+++ api/webhook.js
@@ -7,18 +7,10 @@ import event_left_chat_member from "./events/event_left_chat_member.js";
 
 connectToDB("trinity-podcast-bot");
 
-export default async (_, res) => {
-  const webhookUrl = "https://trinity-podcast-bot.vercel.app/api/webhook";
-  bot.start(cmd_start);
-  bot.on("new_chat_members", event_new_chat_members);
-  await bot.launch({
-    webhook: {
-      domain: webhookUrl,
-      port: process.env.PORT || 3000,
-    },
-  });
-  // bot.on("left_chat_member", event_left_chat_member);
-  // bot.command("soldier-details", cmd_soldier_details);
+bot.start(cmd_start);
+bot.on("new_chat_members", event_new_chat_members);
+// bot.on("left_chat_member", event_left_chat_member);
+// bot.command("soldier-details", cmd_soldier_details);
 
   // bot
   //   .launch()
@@ -28,5 +20,4 @@ export default async (_, res) => {
   //   .catch((err) => {
   //     console.error("An error occured while starting the bot: ", err);
   //   });
-  res.send("Bot is running");
 };
