85bbf6c3f8dfeb3b3ce5719aacf8a81a9c0ee72d
Skeby <103804946+Skeby@users.noreply.github.com>
2024-01-25 20:16:33 +0100

    fix: export webhook

--- api/webhook.js
+++ api/webhook.js
@@ -7,16 +7,18 @@ import event_left_chat_member from "./events/event_left_chat_member.js";
 
 connectToDB("trinity-podcast-bot");
 
-bot.start(cmd_start);
-bot.on("new_chat_members", event_new_chat_members);
-// bot.on("left_chat_member", event_left_chat_member);
-// bot.command("soldier-details", cmd_soldier_details);
+export default () => {
+  bot.start(cmd_start);
+  bot.on("new_chat_members", event_new_chat_members);
+  // bot.on("left_chat_member", event_left_chat_member);
+  // bot.command("soldier-details", cmd_soldier_details);
 
-// bot
-//   .launch()
-//   .then(() => {
-//     console.log("Bot started");
-//   })
-//   .catch((err) => {
-//     console.error("An error occured while starting the bot: ", err);
-//   });
+  // bot
+  //   .launch()
+  //   .then(() => {
+  //     console.log("Bot started");
+  //   })
+  //   .catch((err) => {
+  //     console.error("An error occured while starting the bot: ", err);
+  //   });
+};
