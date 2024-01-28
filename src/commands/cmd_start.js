4830d60bd967a018915bc109ea8f6152f8b4b394
Skeby <103804946+Skeby@users.noreply.github.com>
2024-01-28 23:30:39 +0100

    feat: change reply messages

--- src/commands/cmd_start.js
+++ src/commands/cmd_start.js
@@ -1,6 +1,12 @@
 const cmd_start = (ctx) => {
+  const username =
+    ctx.chat.type === "private" ? ctx.chat.username : ctx.from.username;
+  const firstName =
+    ctx.chat.type === "private" ? ctx.chat.first_name : ctx.from.first_name;
   ctx.reply(
-    "Welcome to Trinity Podcast Bot\nSend /soldierdetails to view your details"
+    `Hello ${
+      username ? `@${username}` : firstName
+    }\nWelcome to the Trinity Army Camp, a community designed to help you become the original you.\n#BecomeTheOriginalYou\n\nSend /soldierdetails to get your Trinity Army ID`
   );
 };
 
