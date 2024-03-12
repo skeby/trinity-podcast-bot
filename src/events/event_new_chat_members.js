79b8ce6ee30ee535ec77a633e7aac59f3f8a3203
Skeby <103804946+Skeby@users.noreply.github.com>
2024-03-12 20:46:09 +0100

    fix: account for multiple users joining at the same time and displaying the correct username or first name

--- src/events/event_new_chat_members.js
+++ src/events/event_new_chat_members.js
@@ -3,10 +3,10 @@ import errorHandler from "../errorHandler.js";
 
 const newUserReply = (
   ctx,
+  firstName,
+  username,
   { communityNumber, squad, platoon, company, battalion }
 ) => {
-  const username = ctx.from.username;
-  const firstName = ctx.from.first_name;
   const numberInSquad = communityNumber % 10 === 0 ? 10 : communityNumber % 10;
   ctx.reply(
     `Welcome to the Trinity Army Camp 🎪, ${
@@ -17,30 +17,29 @@ const newUserReply = (
 
 export default (ctx) => {
   try {
-    let userCount;
-    getUserCount().then((count) => {
-      userCount = count;
-      const user = ctx.message.new_chat_members[0];
-
-      // Exit function if user is a bot
-      if (user.is_bot) return;
-
-      userCount++;
-      const userObj = {
-        communityNumber: userCount,
-        userId: user.id,
-        squad: Math.ceil(userCount / 10),
-        platoon: Math.ceil(userCount / 50),
-        company: Math.ceil(userCount / 1000),
-        battalion: Math.ceil(userCount / 100000),
-      };
-      getUser(user.id).then((existingUser) => {
-        if (!existingUser) {
-          postUser(userObj);
-          newUserReply(ctx, userObj);
-        } else {
-          newUserReply(ctx, existingUser);
-        }
+    const newChatMembers = ctx.message.new_chat_members;
+    getUserCount().then((userCount) => {
+      newChatMembers.forEach((newChatMember) => {
+        // Exit function if user is a bot
+        if (newChatMember.is_bot) return;
+        const user = newChatMember;
+        userCount++;
+        const userObj = {
+          communityNumber: userCount,
+          userId: user.id,
+          squad: Math.ceil(userCount / 10),
+          platoon: Math.ceil(userCount / 50),
+          company: Math.ceil(userCount / 1000),
+          battalion: Math.ceil(userCount / 100000),
+        };
+        getUser(user.id).then((existingUser) => {
+          if (!existingUser) {
+            postUser(userObj);
+            newUserReply(ctx, user.first_name, user.username, userObj);
+          } else {
+            newUserReply(ctx, user.first_name, user.username, existingUser);
+          }
+        });
       });
     });
   } catch (err) {
